4d0bb38c32cbb76f61fbf85dd8af3a80a23e5bc5
Matt Michie <mmichie@gmail.com>
2023-10-31 10:55:09 -0700

    Refactor src/mime.cc for modern C++ standards and clarity
    - Use std:: namespace prefix for standard library types like string, vector, and ifstream.
    - Simplify string manipulations and loop counters.
    - Add explicit return true at the end of readMimeConfig to indicate successful execution.
    - Update comments for better readability.
    
    These changes are aimed at making the code more idiomatic and easier to read, while also adhering to
    modern C++ best practices.

--- src/mime.cc
+++ src/mime.cc
@@ -1,44 +1,44 @@
 #include "mime.h"
 
-bool Mime::readMimeConfig(string filename) {
-    vector<string> tokens;
+bool Mime::readMimeConfig(std::string filename) {
+    std::vector<std::string> tokens;
 
-    ifstream file(filename.c_str());
+    std::ifstream file(filename);
     if (!file.is_open()) {
-        cerr << "Error: can't open " << filename << endl;
+        std::cerr << "Error: can't open " << filename << std::endl;
         return false;
     }
 
-    string tmp_line;
-    while (!getline(file, tmp_line).eof()) {
+    std::string tmp_line;
+    while (std::getline(file, tmp_line)) {
         tokens.clear();
 
-        // if not comment, process
+        // if not a comment, process
         if (tmp_line[0] != '#') {
 
             // tokenize the line
-            string buf;
-            stringstream ss(tmp_line);
+            std::string buf;
+            std::stringstream ss(tmp_line);
             while (ss >> buf)
                 tokens.push_back(buf);
 
-            for (int j = tokens.size(); j > 1; j--) {
+            for (std::size_t j = tokens.size(); j > 1; --j) {
                 this->mimemap[tokens[j - 1]] = tokens[0];
             }
         }
     }
 
     file.close();
+    return true; // Indicate successful execution
 }
 
-string Mime::getMimeFromExtension(string filename) {
-    // Find the extension (assumes the extension is whatever follows the last
-    // '.')
-    string file_extension =
-        filename.substr(filename.rfind(".") + 1, filename.length());
+std::string Mime::getMimeFromExtension(std::string filename) {
+    // Find the extension (assumes the extension is whatever follows the last '.')
+    std::string file_extension = filename.substr(filename.rfind(".") + 1);
 
     if (this->mimemap.find(file_extension) == this->mimemap.end())
         return "text/plain";
     else
         return this->mimemap[file_extension];
 }
+
