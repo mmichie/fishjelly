650ae8c95f8610346a66351275342d8df8a80cd5
Matt Michie <mmichie@gmail.com>
2023-10-30 22:03:38 -0700

    Reformat cgi.cc for consistent 4-space indentation
    - Updated indentation from a mix of spaces and tabs to consistent 4-space indents.
    - Adjusted line breaks for better readability.

--- src/cgi.cc
+++ src/cgi.cc
@@ -11,56 +11,54 @@
                            "SERVER_PORT" | "SERVER_PROTOCOL" |
                            "SERVER_SOFTWARE" | scheme |*/
 void Cgi::setupEnv(map<string, string> headermap) {
-	
-	if (headermap.find("AUTH_TYPE") != headermap.end()) {	
-		setenv("AUTH_TYPE", headermap["AUTH_TYPE"].c_str(), 1);
-	}
-	
-	if (headermap.find("CONTENT_LENGTH") != headermap.end()) {
-		setenv("CONTENT_LENGTH", headermap["CONTENT_LENGTH"].c_str(), 1);
-	}
-	
-	setenv("GATEWAY_INTERFACE", "CGI/1.1", 1);
-	
-	if (headermap.find("QUERY_STRING") != headermap.end()) {
-		setenv("QUERY_STRING", headermap["QUERY_STRING"].c_str(), 1);
-	}
-	
-	setenv("REQUEST_METHOD", "GET", 1);
-	setenv("SERVER_PROTOCOL", "HTTP/1.1", 1);
-	setenv("SERVER_SOFTWARE", "SHELOB/3.14", 1);
-	
+
+    if (headermap.find("AUTH_TYPE") != headermap.end()) {
+        setenv("AUTH_TYPE", headermap["AUTH_TYPE"].c_str(), 1);
+    }
+
+    if (headermap.find("CONTENT_LENGTH") != headermap.end()) {
+        setenv("CONTENT_LENGTH", headermap["CONTENT_LENGTH"].c_str(), 1);
+    }
+
+    setenv("GATEWAY_INTERFACE", "CGI/1.1", 1);
+
+    if (headermap.find("QUERY_STRING") != headermap.end()) {
+        setenv("QUERY_STRING", headermap["QUERY_STRING"].c_str(), 1);
+    }
+
+    setenv("REQUEST_METHOD", "GET", 1);
+    setenv("SERVER_PROTOCOL", "HTTP/1.1", 1);
+    setenv("SERVER_SOFTWARE", "SHELOB/3.14", 1);
 }
 
-bool Cgi::executeCGI(string filename, int accept_fd, map<string, string> headermap) {
+bool Cgi::executeCGI(string filename, int accept_fd,
+                     map<string, string> headermap) {
     int pid;
-	char current_path[MAXPATHLEN];
-	ostringstream buffer;
-	string fullpath;
-	
+    char current_path[MAXPATHLEN];
+    ostringstream buffer;
+    string fullpath;
+
     pid = fork();
 
     if (pid < 0) {
         perror("ERROR on fork");
-	}
+    }
 
     /* Child */
-    if (pid == 0)  {
-    	setupEnv(headermap);
+    if (pid == 0) {
+        setupEnv(headermap);
 
-		dup2(accept_fd, STDOUT_FILENO);
-		
-		filename = filename.substr(7);
-		printf("HTTP/1.1 200 OK\r\n");
-		
-		getcwd(current_path, MAXPATHLEN);
-		buffer << current_path << "/htdocs/" << filename;
-		fullpath = buffer.str();
-		execlp(fullpath.c_str(), filename.c_str(), 0);
-		
-		perror("CGI error");
-		exit(1);
-	}
+        dup2(accept_fd, STDOUT_FILENO);
 
+        filename = filename.substr(7);
+        printf("HTTP/1.1 200 OK\r\n");
+
+        getcwd(current_path, MAXPATHLEN);
+        buffer << current_path << "/htdocs/" << filename;
+        fullpath = buffer.str();
+        execlp(fullpath.c_str(), filename.c_str(), 0);
+
+        perror("CGI error");
+        exit(1);
+    }
 }
-
